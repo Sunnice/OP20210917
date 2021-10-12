d1938863fae8710e35ac933b8d40d833cc87753e
Sunnice <44739306+Sunnice@users.noreply.github.com>
2021-10-12 23:04:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,7 +16,7 @@ git clone https://github.com/fw876/helloworld.git package/helloworld
 git clone https://github.com/ElonH/Rclone-OpenWrt.git package/rclone
 git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/koolproxyR
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
-git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic.git package/unblockneteasemusicadvance
+git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/unblockneteasemusicadvance
 git clone https://github.com/Lienol/openwrt-package.git package/lienol
 git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/adguardhome
